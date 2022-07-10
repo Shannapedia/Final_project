b623ed523fb6c0865655bd29e339f082bc9e4f86
Shannapedia <96964900+Shannapedia@users.noreply.github.com>
2022-07-09 19:05:06 -0600

    created an action and game_ove file and class

---
+++ game.py/scripting/game_over.py
@@ -0,0 +1,54 @@
+from casting.actor import Actor
+from scripting.action import Action
+from shared.point import Point
+from directing.director import Director
+
+
+class HandleCollisionsAction(Action):
+    """
+    An update action that handles interactions between the actors.
+    
+    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
+    with the food, or the snake collides with its segments, or the game is over.
+
+    Attributes:
+        _is_game_over (boolean): Whether or not the game is over.
+    """
+
+    def __init__(self):
+        """Constructs a new HandleCollisionsAction."""
+        self._is_game_over = False
+
+def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            self._handle_game_over(cast)
+            
+def _handle_game_over(self, cast):
+        """Shows the 'game over' message and turns the snake and food white if the game is over.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        if self._is_game_over:
+            snake = cast.get_first_actor("snakes")
+            segments = snake.get_segments()
+            food = cast.get_first_actor("foods")
+
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+            position = Point(x, y)
+
+            message = Actor()
+            message.set_text("Game Over!")
+            message.set_position(position)
+            cast.add_actor("messages", message)
+
+            for segment in segments:
+                segment.set_color(constants.WHITE)
+            food.set_color(constants.WHITE)
