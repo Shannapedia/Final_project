ee2aeaa27ae7d04e2cb2a592bd5f44804b1a9887
Shannapedia <96964900+Shannapedia@users.noreply.github.com>
2022-07-13 22:02:56 -0600

    clean score from unsed code

--- game.py/casting/score.py
+++ game.py/casting/score.py
@@ -1,37 +1,19 @@
 from casting.actor import Actor
 
-
 class Score(Actor):
-    """
-    An item of cultural or historical interest. 
-    
-    The responsibility of an Artifact is to provide a message about itself.
-
-    Attributes:
-        _message (string): A short description about the artifact.
-    """
+    """ Score provide a message to count the score. """
     def __init__(self):
         super().__init__()
         self._message = 0
         
     def get_message(self):
-        """Gets the artifact's message.
-        
-        Returns:
-            string: The message.
-        """
+        """ Gets the score message. """
         return self._message
     
     def set_message(self, message):
-        """Updates the message to the given one.
-        
-        Args:
-            message (string): The given message.
-        """
+        """ Updates the message to the given one. """
         self._message = message
 
     def get_text(self):
-        """
-        Gets artifact's text
-        """
+        """ Gets score's text. """
         return self._text
